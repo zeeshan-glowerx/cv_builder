f314ac3110df0e6bafeda0271fc5a196d5384dcb
Zeeshan Noor <zeeshan.noor@glowerx.com>
2022-03-18 11:12:34 +0500

    download resume into PDF

--- src/page/resume.js
+++ src/page/resume.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import CommonForm from '../components/Forms'
 import Content from '../components/Content'
+import GenericPdfDownloader from '../helper/downloadPdf'
 const Resume = () => {
   return (
+    <>
     <div style={{display:'flex'}}>
       <Content/>
       <CommonForm />
     </div>
+    <GenericPdfDownloader/>
+    </>
   )
 }
 
